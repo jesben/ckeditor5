43f6e9fe2cede89eb9d194f0e954bd9eac28989f
Jesper Bendtsen <jesben@kk.dk>
2021-08-11 21:38:19 +0200

    .

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -42,175 +42,377 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 
+import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak';
+
+// Placholder plugin
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import { toWidget, viewToModelPositionOutsideModelElement } from '@ckeditor/ckeditor5-widget/src/utils';
+import Widget from '@ckeditor/ckeditor5-widget/src/widget';
+import Command from '@ckeditor/ckeditor5-core/src/command';
+
+import { addListToDropdown, createDropdown } from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
+import Collection from '@ckeditor/ckeditor5-utils/src/collection';
+import Model from '@ckeditor/ckeditor5-ui/src/model';
+
+class Placeholder extends Plugin {
+    static get requires() {
+        return [PlaceholderEditing, PlaceholderUI];
+    }
+}
+
+class PlaceholderCommand extends Command {
+    execute({ value }) {
+        const editor = this.editor;
+        const selection = editor.model.document.selection;
+
+        editor.model.change(writer => {
+            // Create a <placeholder> elment with the "name" attribute (and all the selection attributes)...
+            const placeholder = writer.createElement('placeholder', {
+                ...Object.fromEntries(selection.getAttributes()),
+                name: value
+            });
+
+            // ... and insert it into the document.
+            editor.model.insertContent(placeholder);
+
+            // Put the selection on the inserted element.
+            writer.setSelection(placeholder, 'on');
+        });
+    }
+
+    refresh() {
+        const model = this.editor.model;
+        const selection = model.document.selection;
+
+        const isAllowed = model.schema.checkChild(selection.focus.parent, 'placeholder');
+
+        this.isEnabled = isAllowed;
+    }
+}
+
+class PlaceholderUI extends Plugin {
+    init() {
+        const editor = this.editor;
+        const t = editor.t;
+        const placeholderNames = editor.config.get('placeholderConfig.types');
+
+        // The "placeholder" dropdown must be registered among the UI components of the editor
+        // to be displayed in the toolbar.
+        editor.ui.componentFactory.add('placeholder', locale => {
+            const dropdownView = createDropdown(locale);
+
+            // Populate the list in the dropdown with items.
+            addListToDropdown(dropdownView, getDropdownItemsDefinitions(placeholderNames));
+
+            dropdownView.buttonView.set({
+                // The t() function helps localize the editor. All strings enclosed in t() can be
+                // translated and change when the language of the editor changes.
+                label: t('Indsæt nøgleord'),
+                tooltip: true,
+                withText: true
+            });
+
+            // Disable the placeholder button when the command is disabled.
+            const command = editor.commands.get('placeholder');
+            dropdownView.bind('isEnabled').to(command);
+
+            // Execute the command when the dropdown item is clicked (executed).
+            this.listenTo(dropdownView, 'execute', evt => {
+                editor.execute('placeholder', { value: evt.source.commandParam });
+                editor.editing.view.focus();
+            });
+
+            return dropdownView;
+        });
+    }
+}
+
+function getDropdownItemsDefinitions(placeholderNames) {
+    const itemDefinitions = new Collection();
+
+    for (const name of placeholderNames) {
+        const definition = {
+            type: 'button',
+            model: new Model({
+                commandParam: name,
+                label: name,
+                withText: true
+            })
+        };
+
+        // Add the item definition to the collection.
+        itemDefinitions.add(definition);
+    }
+
+    return itemDefinitions;
+}
+
+class PlaceholderEditing extends Plugin {
+    static get requires() {
+        return [Widget];
+    }
+
+    init() {
+        console.log('PlaceholderEditing#init() got called');
+
+        this._defineSchema();
+        this._defineConverters();
+
+        this.editor.commands.add('placeholder', new PlaceholderCommand(this.editor));
+
+        this.editor.editing.mapper.on(
+            'viewToModelPosition',
+            viewToModelPositionOutsideModelElement(this.editor.model, viewElement => viewElement.hasClass('placeholder'))
+        );
+        this.editor.config.define('placeholderConfig', {
+            types: ['date', 'first name', 'surname']
+        });
+    }
+
+    _defineSchema() {
+        const schema = this.editor.model.schema;
+
+        schema.register('placeholder', {
+            // Allow wherever text is allowed:
+            allowWhere: '$text',
+
+            // The placeholder will act as an inline node:
+            isInline: true,
+
+            // The inline widget is self-contained so it cannot be split by the caret and it can be selected:
+            isObject: true,
+
+            // The inline widget can have the same attributes as text (for example linkHref, bold).
+            allowAttributesOf: '$text',
+
+            // The placeholder can have many types, like date, name, surname, etc:
+            allowAttributes: ['name']
+        });
+    }
+
+    _defineConverters() {
+        const conversion = this.editor.conversion;
+
+        conversion.for('upcast').elementToElement({
+            view: {
+                name: 'span',
+                classes: ['placeholder']
+            },
+            model: (viewElement, { writer: modelWriter }) => {
+                // Extract the "name" from "{name}".
+                const name = viewElement.getChild(0).data.slice(1, -1);
+
+                return modelWriter.createElement('placeholder', { name });
+            }
+        });
+
+        conversion.for('editingDowncast').elementToElement({
+            model: 'placeholder',
+            view: (modelItem, { writer: viewWriter }) => {
+                const widgetElement = createPlaceholderView(modelItem, viewWriter);
+
+                // Enable widget handling on a placeholder element inside the editing view.
+                return toWidget(widgetElement, viewWriter);
+            }
+        });
+
+        conversion.for('dataDowncast').elementToElement({
+            model: 'placeholder',
+            view: (modelItem, { writer: viewWriter }) => createPlaceholderView(modelItem, viewWriter)
+        });
+
+        // Helper method for both downcast converters.
+        function createPlaceholderView(modelItem, viewWriter) {
+            const name = modelItem.getAttribute('name');
+
+            const placeholderView = viewWriter.createContainerElement('span', {
+                class: 'placeholder'
+            }, {
+                isAllowedInsideAttributeElement: true
+            });
+
+            // Insert the placeholder name (as a text).
+            const innerText = viewWriter.createText('{' + name + '}');
+            viewWriter.insert(viewWriter.createPositionAt(placeholderView, 0), innerText);
+
+            return placeholderView;
+        }
+    }
+}
+
 class ClassicEditor extends ClassicEditorBase { }
 class DecoupledEditor extends DecoupledEditorBase { }
 
 // Plugins to include in the build.
 
 ClassicEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	CKFinder,
-	CloudServices,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar,
-	TextTransformation
+    Essentials,
+    UploadAdapter,
+    Autoformat,
+    Bold,
+    Italic,
+    BlockQuote,
+    CKFinder,
+    CloudServices,
+    EasyImage,
+    Heading,
+    Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    Link,
+    List,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    Table,
+    TableToolbar,
+    TextTransformation
 ];
 
 DecoupledEditor.builtinPlugins = [
-	Essentials,
-	Alignment,
-	FontSize,
-	FontFamily,
-	FontColor,
-	FontBackgroundColor,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	Strikethrough,
-	Underline,
-	BlockQuote,
-	CKFinder,
-	CloudServices,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageResize,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
-	IndentBlock,
-	Link,
-	List,
-	ListStyle,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar,
-	TextTransformation
+    Essentials,
+    Alignment,
+    FontSize,
+    FontFamily,
+    FontColor,
+    FontBackgroundColor,
+    UploadAdapter,
+    Autoformat,
+    Bold,
+    Italic,
+    Strikethrough,
+    Underline,
+    BlockQuote,
+    CKFinder,
+    CloudServices,
+    EasyImage,
+    Heading,
+    Image,
+    ImageCaption,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Link,
+    List,
+    ListStyle,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
+    Table,
+    TableToolbar,
+    TextTransformation,
+    PageBreak,
+    Placeholder
 ];
 
 // Editor configuration.
 
 ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'uploadImage',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:inline',
-			'imageStyle:block',
-			'imageStyle:side',
-			'|',
-			'toggleImageCaption',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+    toolbar: {
+        items: [
+            'heading',
+            '|',
+            'bold',
+            'italic',
+            'link',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'outdent',
+            'indent',
+            '|',
+            'uploadImage',
+            'blockQuote',
+            'insertTable',
+            'mediaEmbed',
+            'undo',
+            'redo'
+        ]
+    },
+    image: {
+        toolbar: [
+            'imageStyle:inline',
+            'imageStyle:block',
+            'imageStyle:side',
+            '|',
+            'toggleImageCaption',
+            'imageTextAlternative'
+        ]
+    },
+    table: {
+        contentToolbar: [
+            'tableColumn',
+            'tableRow',
+            'mergeTableCells'
+        ]
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: 'en'
 };
 
 DecoupledEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'fontfamily',
-			'fontsize',
-			'fontColor',
-			'fontBackgroundColor',
-			'|',
-			'bold',
-			'italic',
-			'underline',
-			'strikethrough',
-			'|',
-			'alignment',
-			'|',
-			'numberedList',
-			'bulletedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'link',
-			'blockquote',
-			'uploadImage',
-			'insertTable',
-			'mediaEmbed',
-			'|',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		resizeUnit: 'px',
-		toolbar: [
-			'imageStyle:inline',
-			'imageStyle:wrapText',
-			'imageStyle:breakText',
-			'|',
-			'toggleImageCaption',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+    toolbar: {
+        items: [
+            'heading',
+            '|',
+            //'fontfamily',
+            'fontsize',
+            'fontColor',
+            'fontBackgroundColor',
+            '|',
+            'bold',
+            'italic',
+            'underline',
+            'strikethrough',
+            '|',
+            'numberedList',
+            'bulletedList',
+            '|',
+            'alignment',
+            'outdent',
+            'indent',
+            'pageBreak',
+            '|',
+            'link',
+            'blockquote',
+            'uploadImage',
+            'insertTable',
+            'mediaEmbed',
+            '|',
+            'undo',
+            'redo',
+            '|',
+            'placeholder',
+        ]
+    },
+    image: {
+        resizeUnit: 'px',
+        toolbar: [
+            'imageStyle:inline',
+            'imageStyle:wrapText',
+            'imageStyle:breakText',
+            '|',
+            'toggleImageCaption',
+            'imageTextAlternative'
+        ]
+    },
+    table: {
+        contentToolbar: [
+            'tableColumn',
+            'tableRow',
+            'mergeTableCells'
+        ]
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: 'en'
 };
 
 export default {
-	ClassicEditor, DecoupledEditor
+    ClassicEditor, DecoupledEditor
 };
